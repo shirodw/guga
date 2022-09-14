81d52801a90e5bec3066ca20067076ee1705bb88
shirodw <74607031+shirodw@users.noreply.github.com>
2022-09-14 12:57:51 +0300

    wrf

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World my!" << std::endl;
-    std::cout << "Hello, World my!" << std::endl;
+    std::cout << "Hello, Worrfrfrfrld my!" << std::endl;
     return 0;
 }
