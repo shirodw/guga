c24e729c1cfd7465c727ec7528a650061d00601a
shirodw <74607031+shirodw@users.noreply.github.com>
2022-09-14 12:58:41 +0300

    lokalol

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World my!" << std::endl;
+    std::cout << "Hello, World erf3erfemy!" << std::endl;
     std::cout << "Hello, Worrfrfrfrld my!" << std::endl;
     return 0;
 }
