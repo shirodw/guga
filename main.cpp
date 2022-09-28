6549315def29a1c4b277d0bd5dd24f9d194ddd7d
shirodw <74607031+shirodw@users.noreply.github.com>
2022-09-28 12:28:44 +0300

    hbvghjkjhj

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+    if (argc<3){
+        return 1;
+    }
+    int n=atoi(argv[1]);
+    {
+        int n=44;
+    }
+   cout << argc<<'\n';
+   for(size_t i=0;i<n;++i){
+       cout <<argv[2]<<'\n';
+   }
+   int arr[5];
 
-int main() {
-    std::cout << "Hello, World erf3erfemy!" << std::endl;
-    std::cout << "Hello, Worrfrfrfrld my!" << std::endl;
-    return 0;
 }
 //lolkookokkojo
