f46e432f4ebed6445018e883072c82e0e740c718
shirodw <74607031+shirodw@users.noreply.github.com>
2022-09-28 13:00:09 +0300

    why_]work

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char** argv) {
-    if (argc<3){
-        return 1;
-    }
-    int n=atoi(argv[1]);
-    {
-        int n=44;
-    }
-   cout << argc<<'\n';
-   for(size_t i=0;i<n;++i){
-       cout <<argv[2]<<'\n';
-   }
-   int arr[4];
+    std::cout<<"heellow ooddl"<< std::end
 
 }
 //lolkookokkojo
