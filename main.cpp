fe7a823541f7023b5f22c4cf76bac5703de59559
shirodw <74607031+shirodw@users.noreply.github.com>
2022-09-28 12:11:34 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+    if (argc<3){
+        return 1;
+    }
+    int n=atoi(argv[1]);
+    {
+        int n=44;
+    }
+   cout << argc<<'\n';
+   for(size_t i=0;i<n;++i){
+       cout <<argv[2]<<'\n';
+   }
+   int arr[5];
+
+}
+//lolkookokkojo
